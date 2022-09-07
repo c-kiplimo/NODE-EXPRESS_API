a2692a67e9509c1e6a8f21beea423edff95cb0db
LimoCollins <90177693+LimoCollins@users.noreply.github.com>
2022-09-08 00:21:27 +0300

    little learning

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 import express from 'express';
 
+import { v4 as uuidv4 } from "uuid";
+
 
 const router = express.Router();
 
+let users = [
+ 
+]
+
 router.get('/',(req,res)=>{
-    res.send('Hello');
-} )
+    console.log(users);
+
+    res.send(users);
+} );
+
+
+router.post('/');
+
+router.get('/:id',(req,res) =>{
+    const { id } =req.params;
+    const foundUser=users.find((user)=> user.id==id)
+
+    res.send(foundUser);
+})
+router.delete('/:id',(req,res)=>{
+    const { id } = req.params;
+    users=users.filter((user) => user.id != id);
+    res.send(`User with id ${id} deleted from the database.`)
+}); 
+router.patch('/:id',(req,res) =>{
+    const { id }  =req.params;
+    const {FirstName,LastName,age}=req.body;
+    const user =users.find(()=>user.id ==id); 
+    if(FirstName){
+        user.FirstName=FirstName;
+    }
+    if(LasttName){
+        user.LastName=LastName;
+    }
+    if(age){
+        user.age=age;
+    }
+
+    res.send(`User with the id ${id} has been updated`);
+
+
+
+})
 export default router;
